dcefae1750545d2484b40a5f7ceb3343fa497fa5
Vana Brajcic <vana.brajcic@gmail.com>
2017-06-03 12:09:58 +0200

    Add files via upload

---
+++ LikePaint/SaveDelete.cs
@@ -0,0 +1,80 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace LikePaint
+{
+    public partial class MainWindow : MetroWindow
+    {
+        private void Save(string filename)
+        {
+            /*if (filename == null)
+                return;
+
+            // Save current canvas transform
+            Transform transform = canvas.LayoutTransform;
+            // reset current transform (in case it is scaled or rotated)
+            canvas.LayoutTransform = null;
+
+            // Get the size of canvas
+            Size size = new Size(canvas.Width, canvas.Height);
+            // Measure and arrange the surface
+            // VERY IMPORTANT
+            canvas.Measure(size);
+            canvas.Arrange(new Rect(size));
+
+            // Create a render bitmap and push the surface to it
+            RenderTargetBitmap renderBitmap =
+              new RenderTargetBitmap(
+                (int)size.Width,
+                (int)size.Height,
+                96d,
+                96d,
+                PixelFormats.Pbgra32);
+            renderBitmap.Render(canvas);
+
+            // Create a file stream for saving image
+            using (FileStream outStream = new FileStream(filename, FileMode.Create))
+            {
+                // Use png encoder for our data
+                PngBitmapEncoder encoder = new PngBitmapEncoder();
+                // push the rendered bitmap to it
+                encoder.Frames.Add(BitmapFrame.Create(renderBitmap));
+                // save the data to the stream
+                encoder.Save(outStream);
+            }
+
+            // Restore previously saved layout
+            canvas.LayoutTransform = transform;
+        }*/
+
+            Rect rect = new Rect(canvas.RenderSize);
+            RenderTargetBitmap rtb = new RenderTargetBitmap((int)rect.Right,
+              (int)rect.Bottom, 96d, 96d, System.Windows.Media.PixelFormats.Default);
+            rtb.Render(canvas);
+            //endcode as PNG
+            BitmapEncoder pngEncoder = new PngBitmapEncoder();
+            pngEncoder.Frames.Add(BitmapFrame.Create(rtb));
+
+            //save to memory stream
+            System.IO.MemoryStream ms = new System.IO.MemoryStream();
+
+            pngEncoder.Save(ms);
+            ms.Close();
+            System.IO.File.WriteAllBytes(filename, ms.ToArray());
+            Console.WriteLine("Done");
+        }
+
+        private void Delete()
+        {
+            canvas.Children.Clear();
+        }
+    }
+}
